7b41d54d426a9cb1415ed6a5d3583fd235d9817d
Mihai Mijea <mmihai@suitsupply.com>
2017-12-21 06:40:23 +0100

    service-worker-lab

--- service-worker-lab/app/below/service-worker.js
+++ service-worker-lab/app/below/service-worker.js
@@ -13,13 +13,25 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-(function() {
-  'use strict';
+(function () {
+	'use strict';
 
-  // TODO - 3.1: Add install and activate event listeners
+	// TODO - 3.1: Add install and activate event listeners
+	self.addEventListener('install', function (event) {
+		console.log('Service worker installing...');
+		//TODO 3.4: Skip waiting
+		self.skipWaiting();
+	});
 
-  // TODO - 3.3: Add a comment to change the service worker
+	self.addEventListener('activate', function (event) {
+		console.log('Service worker activating');
+	});
 
-  // TODO - 4: Add fetch listener
+	// I'm a new service worker
+
+	// TODO - 4: Add fetch listener
+	self.addEventListener('fetch', function (event) {
+		console.log('Fetching: ', event.request.url);
+	});
 
 })();
