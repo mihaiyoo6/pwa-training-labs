1d897d49aa909215b1c0582263f9583a420353f8
Mihai Mijea <mmihai@suitsupply.com>
2017-12-21 06:39:13 +0100

    fetch-api-lab

--- fetch-api-lab/app/echo-servers/echo-server-cors.js
+++ fetch-api-lab/app/echo-servers/echo-server-cors.js
@@ -1,27 +1,27 @@
 var http = require('http');
 var port = 5000;
 
-http.createServer(function(request, response) {
+http.createServer(function (request, response) {
 
-  // enable CORS
-  response.setHeader('Access-Control-Allow-Origin', '*');
-  response.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
-  response.setHeader('Access-Control-Allow-Headers', 'X-CUSTOM, Content-Type');
+	// enable CORS
+	response.setHeader('Access-Control-Allow-Origin', '*');
+	response.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+	response.setHeader('Access-Control-Allow-Headers', 'X-CUSTOM, Content-Type, Y-Custom');
 
-  request.on('data', function(message) {
-    // echo back request with headers
-    var responseWithHeaders = JSON.stringify(request.headers, null, 1) +
-                              '\n' +
-                              message.toString();
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write(responseWithHeaders);
-    response.end();
-    console.log(responseWithHeaders);
-  });
+	request.on('data', function (message) {
+		// echo back request with headers
+		var responseWithHeaders = JSON.stringify(request.headers, null, 1) +
+      '\n' +
+      message.toString();
+		response.writeHead(200, { 'Content-Type': 'text/plain' });
+		response.write(responseWithHeaders);
+		response.end();
+		console.log(responseWithHeaders);
+	});
 
-  request.on('end', function() {
-    response.end();
-  });
+	request.on('end', function () {
+		response.end();
+	});
 
 }).listen(port);
 
--- fetch-api-lab/app/js/main.js
+++ fetch-api-lab/app/js/main.js
@@ -13,81 +13,129 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-var app = (function() {
-  'use strict';
+var app = (function () {
+	'use strict';
 
-  function logResult(result) {
-    console.log(result);
-  }
+	function logResult(result) {
+		console.log(result);
+	}
 
-  function logError(error) {
-    console.log('Looks like there was a problem: \n', error);
-  }
+	function logError(error) {
+		console.log('Looks like there was a problem: \n', error);
+	}
 
-  // TODO 2.1a
+	if (!('fetch' in window)) {
+		console.log('Fetch API not found, try including the polyfill');
+		return;
+	}
 
-  function fetchJSON() {
-    // TODO 2.1b
-  }
+	function fetchJSON() {
+		fetch('examples/animals.json')
+			.then(validateResponse)
+			.then(readResponseAsJSON)
+			.then(logResult)
+			.catch(logError);
+	}
 
-  function validateResponse(response) {
-    // TODO 2.3
-  }
+	function validateResponse(response) {
+		if (!response.ok) {
+			throw Error(response.statusText);
+		}
+		return response;
+	}
 
-  function readResponseAsJSON(response) {
-    // TODO 2.4
-  }
+	function readResponseAsJSON(response) {
+		return response.json();
+	}
 
-  function showImage(responseAsBlob) {
-    //  TODO 3a
-  }
+	function showImage(responseAsBlob) {
+		const container = document.getElementById('container');
+		const imgElem = document.createElement('img');
+		container.appendChild(imgElem);
+		const imgUrl = URL.createObjectURL(responseAsBlob);
+		imgElem.src = imgUrl;
+	}
 
-  function readResponseAsBlob(response) {
-    // TODO 3b
-  }
+	function readResponseAsBlob(response) {
+		return response.blob();
+	}
 
-  function fetchImage() {
-    // TODO 3c
-  }
+	function fetchImage() {
+		fetch('examples/kitten.jpg')
+			.then(validateResponse)
+			.then(readResponseAsBlob)
+			.then(showImage)
+			.catch(logError);
+	}
 
-  function showText(responseAsText) {
-    //  TODO 4a
-  }
+	function showText(responseAsText) {
+		const message = document.getElementById('message');
+		message.textContent = responseAsText;
+	}
 
-  function readResponseAsText(response) {
-    // TODO 4b
-  }
+	function readResponseAsText(response) {
+		return response.text();
+	}
 
-  function fetchText() {
-    // TODO 4c
-  }
+	function fetchText() {
+		fetch('examples/words.txt')
+			.then(validateResponse)
+			.then(readResponseAsText)
+			.then(showText)
+			.catch(logError);
+	}
 
-  function headRequest() {
-    // TODO 5.1
-  }
+	function headRequest() {
+		fetch('examples/words.txt', {
+			method: 'HEAD'
+		})
+			.then(validateResponse)
+			.then(logSize)
+			.then(readResponseAsText)
+			.then(logResult)
+			.catch(logError);
 
-  function logSize(response) {
-    // TODO 5.2
-  }
+	}
 
-  /* NOTE: Never send unencrypted user credentials in production! */
-  function postRequest() {
-    // TODO 6.2
-  }
+	function logSize(response) {
+		console.log(response.headers.get('content-length'));
+		return response;
+	}
 
-  // Don't worry if you don't understand this, it's not part of the Fetch API.
-  // We are using the JavaScript Module Pattern to enable unit testing of
-  // our functions.
-  return {
-    readResponseAsJSON: (readResponseAsJSON),
-    readResponseAsBlob: (readResponseAsBlob),
-    readResponseAsText: (readResponseAsText),
-    validateResponse: (validateResponse),
-    fetchJSON: (fetchJSON),
-    fetchImage: (fetchImage),
-    fetchText: (fetchText),
-    headRequest: (headRequest),
-    postRequest: (postRequest)
-  };
+	/* NOTE: Never send unencrypted user credentials in production! */
+	function postRequest() {
+		const formData = new FormData(document.getElementById('myForm'));
+		const customHeaders = new Headers({
+			'Content-type': 'text/plain',
+			'Content-length': 6,
+			'X-Custom': 'hello world',
+			'Y-Custom': 'hello world'
+		});
+
+		fetch('http://localhost:5000/', {
+			method: 'POST',
+			headers: customHeaders,
+			body: formData
+		})
+			.then(validateResponse)
+			.then(readResponseAsText)
+			.then(logResult)
+			.catch(logError);
+	}
+
+	// Don't worry if you don't understand this, it's not part of the Fetch API.
+	// We are using the JavaScript Module Pattern to enable unit testing of
+	// our functions.
+	return {
+		readResponseAsJSON: (readResponseAsJSON),
+		readResponseAsBlob: (readResponseAsBlob),
+		readResponseAsText: (readResponseAsText),
+		validateResponse: (validateResponse),
+		fetchJSON: (fetchJSON),
+		fetchImage: (fetchImage),
+		fetchText: (fetchText),
+		headRequest: (headRequest),
+		postRequest: (postRequest)
+	};
 
 })();
